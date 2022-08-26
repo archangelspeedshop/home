53199cc0fd09d04d2ec0a74f21eb949943e4bc75
Agustin Demousselle <agustin.demousselle@matillion.com>
2022-08-26 15:41:40 +0100

    adds CTA buttons in Home and basic styles and responsiveness

--- src/App/components/Header/Header.jsx
+++ src/App/components/Header/Header.jsx
@@ -5,16 +5,20 @@ import classes from './Header.module.scss'
 const Header = () => {
     return (
         <header className={classes.Header__Container}>
-            <img src={ArchangelLogo} alt="archangel logo"/>
-            <div className={classes.Header__Links_Container}>
-                <div className={classes.Header__Links}>
-                    <Link to="/">Home</Link>
-                    <Link to="about-us">Services</Link>
-                    <Link to="about-us">About Us</Link>
-                    <Link to="about-us">Contact</Link>
-                </div>
-                <div className={classes.Header__CTA}>
-                    <Link to="about-us">Book online</Link>
+            <div className={classes.Header__Left}>
+                <img src={ArchangelLogo} alt="archangel logo"/>
+            </div>
+            <div className={classes.Header__Right}>
+                <div className={classes.Header__Links_Container}>
+                    <div className={classes.Header__Links}>
+                        <Link to="/">Home</Link>
+                        <Link to="services">Services</Link>
+                        <Link to="about-us">About Us</Link>
+                        <Link to="contact">Contact</Link>
+                    </div>
+                    <div className={classes.Header__CTA}>
+                        <Link to="book">Book online</Link>
+                    </div>
                 </div>
             </div>
         </header>
--- src/modules/Home/Home.jsx
+++ src/modules/Home/Home.jsx
@@ -1,6 +1,46 @@
+import classes from './Home.module.scss'
+import MapsLogo from '../../assets/vectors/google-maps-logo.svg'
+import Phone from '../../assets/vectors/phone-icon.svg'
+import Instagram from '../../assets/vectors/instagram-logo.svg'
+
 const Home = () => {
     return (
-        <p>Home</p>
+        <div className={classes.Content}>
+            <div className={classes.Content__Wrapper}>
+                <div className={classes.Content__Left}>
+                    <div className={classes.Ferrari} />
+                </div>
+                <div className={classes.Content__Right}>
+                    <div className={classes.Description}>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quis lacus suscipit, ornare augue sed, malesuada lacus. Nullam velit lectus, porttitor a euismod et, semper eget arcu. Donec vitae accumsan </p>
+                    </div>
+                    <a className={classes.Maps_Wrapper} href="https://goo.gl/maps/scKABvQ37xKSHrB77" target="__blank">
+                        <div className={classes.IconWrapper}>
+                            <img src={MapsLogo} alt="Google Maps Logo"/>
+                        </div>
+                        <div className={classes.TextContainer}>
+                            <p>Find us in Google Maps</p>
+                        </div>
+                    </a>
+                    <a className={classes.Maps_Wrapper} href="tel:01506368150">
+                        <div className={classes.IconWrapper}>
+                            <img src={Phone} alt="Google Maps Logo"/>
+                        </div>
+                        <div className={classes.TextContainer}>
+                            <p>0150 6368150</p>
+                        </div>
+                    </a>
+                    <a className={classes.Maps_Wrapper} href="https://www.instagram.com/archangel_speedshop" target="__blank">
+                        <div className={classes.IconWrapper}>
+                            <img src={Instagram} alt="Instagram Logo"/>
+                        </div>
+                        <div className={classes.TextContainer}>
+                            <p>Find us in Instagram</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
     )
 }
 
